bbd28dc33521ed21ded75b0f5a1d735a29ffdac1
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-07 17:28:06 -0800

    py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" pascal triangle
-"""
-
 def pascal_triangle(n):
-    """Returns Pascal's triangle as a list of lists."""
+    """
+    Generate Pascal's triangle up to the nth row.
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]
-
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        current_row = [1] + [prev_row[j - 1] + prev_row[j] for j in range(1, i)] + [1]
-        triangle.append(current_row)
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
 
     return triangle
+
+if __name__ == "__main__":
+    n = 5
+    triangle = pascal_triangle(n)
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
