d85fc7fee225ede08e7da1cddccb75b619383649
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-24 11:31:42 -0700

    upd

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ single character H. Your text editor
 can execute only two operations in this file: `Copy All` and `Paste`
 """
 
-def minOperations(n: int) -> int:
-    if n <= 1:
-        return n
 
-    operations = 0
-    clipboard = 1  # Initially, the clipboard contains one 'H'
-    result = 1  # Initially, we have one 'H' in the file
+def minOperations(n):
+    if n <= 0:
+        return 0
+    return minOperationsHelper(n, 1, 0, 0)
 
-    while result < n:
-        if n % result == 0:  # If result is a divisor of n, perform Copy All
-            clipboard = result
-            operations += 1
-        result += clipboard  # Paste the content of the clipboard
-        operations += 1
+def minOperationsHelper(n, buffer, clipboard, operations):
+    if buffer == n:
+        return operations
+    if buffer > n:
+        return float('inf')
+    # Copy All
+    if clipboard == 0:
+        return minOperationsHelper(n, buffer, buffer, operations + 1)
+    # Paste
+    return min(
+        minOperationsHelper(n, buffer + clipboard, clipboard, operations + 1),
+        minOperationsHelper(n, buffer + buffer, buffer, operations + 2)
+    )
 
-    return operations
+# Example usage:
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
-# Example usage
-if __name__ == "__main__":
-    n = 4
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
