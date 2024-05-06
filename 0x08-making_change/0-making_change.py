73380f1e69ef4944b3c3f5240ec6e84b51e2448d
mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-05-06 00:36:39 +0000

    upd

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,7 +3,6 @@
 Module for making change using the fewest number of coins
 """
 
-
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given total
@@ -19,17 +18,16 @@ def makeChange(coins, total):
         return 0
 
     # Initialize an array to store the fewest coins needed for each total from 0 to target total
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0  # Base case: 0 coins needed for total of 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # Base case: 0 coins needed for total of 0
 
     # Iterate through each coin value
     for coin in coins:
-        # Update min_coins array for each total from coin value to target total
+        # Update dp array for each total from coin value to target total
         for i in range(coin, total + 1):
-            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return min_coins[total] if min_coins[total] != float('inf') else -1
-
+    return dp[total] if dp[total] != float('inf') else -1
 
 if __name__ == "__main__":
     # Test cases
